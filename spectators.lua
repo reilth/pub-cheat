f253b3d93eee8af1db729f2ab8df8cb1b60609cc
reilth <95547007+reilth@users.noreply.github.com>
2022-01-13 12:05:56 +0100

    Update spectators.lua

--- spectators.lua
+++ spectators.lua
@@ -60,7 +60,7 @@
 		Background.Name = "Background"
 		Background.Parent = Spectators
 		Background.BackgroundColor3 = Color3.fromRGB(0, 0, 0, 0)
-		Background.BorderColor3 = Color3.fromRGB(25, 25 , 25)
+		Background.BorderColor3 = Color3.fromRGB(40, 40, 40)
 		Background.BorderSizePixel = 1
 		Background.Size = UDim2.new(0, 200, 0, 20)
 
