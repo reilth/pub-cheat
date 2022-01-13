35a83521a9af5ab1e92fd5bb699cbc90c60b4aa8
reilth <95547007+reilth@users.noreply.github.com>
2022-01-13 13:25:39 +0100

    Update spectators.lua

--- spectators.lua
+++ spectators.lua
@@ -47,7 +47,7 @@
 		Players.Name = "Players"
 		Players.Parent = Container
 		Players.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-		Players.BackgroundTransparency = 0.000
+		Players.BackgroundTransparency = 1.000
 		Players.BorderColor3 = Color3.fromRGB(0, 0, 0) 
 		Players.BorderSizePixel = 1
 		Players.Position = UDim2.new(0.0196080022, 0, 1.44285719, 0)
